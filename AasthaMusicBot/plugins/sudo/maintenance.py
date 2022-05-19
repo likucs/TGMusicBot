5f64ff0985039842bf3a9bbaa430da3123948af1
Liku cs <83144124+likucs@users.noreply.github.com>
2022-05-19 13:48:16 +0530

    Update maintenance.py

--- AasthaMusicBot/plugins/sudo/maintenance.py
+++ AasthaMusicBot/plugins/sudo/maintenance.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
 # A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
 
-# Kanged By © @Dr_Asad_Ali
+# Kanged  © @Dr_Asad_Ali
 # Rocks © @Shayri_Music_Lovers
 # Owner Asad Ali 
 # Harshit Sharma
